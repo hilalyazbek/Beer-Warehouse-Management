038f3e146c478ea8046c42bec593526320bc98a6
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-03-12 21:56:12 +0300

    added initial migration and seed data. might implement some tweaks down the line

--- src/Iterates.Bwm.Domain/Entities/WholesalerStock.cs
+++ src/Iterates.Bwm.Domain/Entities/WholesalerStock.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Iterates.Bwm.Domain.Entities;
 
-public class BeerStock : EntityBase
+public class WholesalerStock : EntityBase
 {
     public Guid WholesalerId { get; set; }
     public Guid BeerId { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
     public int StockLevel { get; set; }
     public virtual Wholesaler? Wholesaler { get; set; }
