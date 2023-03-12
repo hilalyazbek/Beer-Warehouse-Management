6503e5d1373aa10801068941949c2866255744c3
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-03-12 22:03:50 +0300

    added async generic repository interface and implementation

---
+++ src/Iterates.Bwm.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Iterates.Bwm.Domain.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task RemoveAsync(T entity);
+}
+
---
+++ src/Iterates.Bwm.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Iterates.Bwm.Infrastructure.DbContexts;
+using System.Linq.Expressions;
+using Iterates.Bwm.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iterates.Bwm.Infrastructure.Repositories;
+
+public class AsyncRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly AppDbContext _dbContext;
+
+    public AsyncRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbContext.Set<T>().ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbContext.Set<T>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbContext.Set<T>().Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(T entity)
+    {
+        _dbContext.Set<T>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
+
